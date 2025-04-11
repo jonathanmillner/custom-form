811eb2b7e47ba0d008c9e5c021140fa4b06b5d71
jonathanmilner <jonathanmillner1@gmail.com>
2025-04-11 15:56:28 -0300

    Removed unnecessary utils folder

--- src/utils/formUtils.ts
+++
@@ -1,8 +0,0 @@
-import { FieldsMap, FieldDefinition } from "../types";
-
-export function arrayToFieldsMap(fields: FieldDefinition[]): FieldsMap {
-  return fields.reduce((acc, { id, type, question, required }) => {
-    acc[id] = { type, question, required };
-    return acc;
-  }, {} as FieldsMap);
-}
